45d9715beff4a8a89dcf991b052aa179ad3f122e
Ahmed Reda <ahmedredaabdelhay@gmail.coma>
2026-09-06 23:29:52 +0300

    Finish The Mega Menu And Add The Articles Section And Some Articles (Boxes)

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// "use strict";
+// const myMegaMenuButton = document.getElementById("nav-mega-menu");
+// const myMegaMenu = document.getElementById("mega-menu");
+
+
+// myMegaMenuButton.addEventListener("click" , function () {
+//     myMegaMenu.style.display = "flex";
+// });
+
+// console.log(myMegaMenuButton);
+// console.log(myMegaMenu);
