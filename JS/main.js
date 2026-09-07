d4c71c5f6de66b6f07384426d817dc15c1a11737
Ahmed Reda <ahmedredaabdelhay@gmail.coma>
2026-09-07 21:41:31 +0300

    Add Scroll to Top Button And Some Feats

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,23 @@
 
 // console.log(myMegaMenuButton);
 // console.log(myMegaMenu);
+
+
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 500) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+});
+
+scrollTopBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
